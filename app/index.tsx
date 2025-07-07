098077018f75653eabe54ca1f5e34a51b6d35bcf
Nate <nathaniels2016@gmail.com>
2025-07-07 15:52:27 -0700

    Add text

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+
+	//fetch classes here. 
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text>This page will show all the folders/classes.</Text>
+		</View>
+	);
 }
