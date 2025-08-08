1eb03450c3fb53bb6214b64a60c105a14341bcb0
Nate <nathaniels2016@gmail.com>
2025-08-07 22:16:50 -0700

    Days work

--- app/set/[setId]/index.tsx
+++ app/set/[setId]/index.tsx
@@ -1,5 +1,5 @@
 import FlashCard from '@/components/FlashCard';
-import { Link, Stack, useFocusEffect, useLocalSearchParams } from 'expo-router';
+import { Stack, useFocusEffect, useLocalSearchParams } from 'expo-router';
 import { useSQLiteContext } from 'expo-sqlite';
 import { useCallback, useState } from 'react';
 import { View } from 'react-native';
@@ -53,17 +53,13 @@ export default function SetView() {
 				<Button onPress={() => null}>Flash Card</Button>
 			</View>
 
-			{cards.length > 0 ?
+			{cards.length > 0 &&
 				<>
 					<FlashCard flashCard={cards[viewingIndex]} />
 					<IconButton icon='arrow-left' onPress={() => setIndex(index => --index)} disabled={viewingIndex === 0}></IconButton>
 					<Text>{viewingIndex + 1} / {cards.length}</Text>
 					<IconButton icon='arrow-right' onPress={() => setIndex(index => ++index)} disabled={viewingIndex === cards.length - 1}></IconButton>
 				</>
-				:
-				<Button mode='contained' style={{ backgroundColor: theme.colors.primary }}>
-					<Link style={{ color: 'black' }} href={`/set/create?courseId=${data?.class_id}`}>Create Set</Link>
-				</Button>
 			}
 		</SafeAreaView>
 	)
