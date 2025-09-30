fb0542523b6e9155142a24fa631ff4fb9155f739
sherretten <nathaniels2016@gmail.com>
2025-09-29 18:36:46 -0700

    Add home button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
 import { cardQueries } from '@/db/queries/cardQueries';
 import { classQueries } from '@/db/queries/classQueries';
 import { setQueries } from '@/db/queries/setQueries';
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { SQLiteProvider } from 'expo-sqlite';
 import { Suspense } from 'react';
-import { ActivityIndicator, MD2Colors, PaperProvider } from 'react-native-paper';
+import { ActivityIndicator, Button, MD2Colors, PaperProvider } from 'react-native-paper';
 
 export const unstable_settings = {
 	initialRouteName: 'index',
@@ -23,7 +23,7 @@ export default function RootLayout() {
 		<PaperProvider theme={{ dark: false }}>
 			<Suspense fallback={<ActivityIndicator animating color={MD2Colors.blue200} />}>
 				<SQLiteProvider databaseName='study-app.db' onInit={createDbIfNone} useSuspense>
-					<Stack screenOptions={{ contentStyle: { backgroundColor: '#283444' } }}>
+					<Stack screenOptions={{ contentStyle: { backgroundColor: '#283444' }, headerRight: (props) => <Link {...props} href={`/`}><Button icon='home'>Home</Button></Link> }}>
 						<Stack.Screen name='index' options={{ title: 'Home' }}></Stack.Screen>
 						<Stack.Screen name='classes' options={{ title: 'Courses', headerBackButtonMenuEnabled: true }}></Stack.Screen>
 						<Stack.Screen name='set/[setId]' options={{ headerShown: false }} />
--- app/set/[setId]/_layout.tsx
+++ app/set/[setId]/_layout.tsx
@@ -1,9 +1,14 @@
-import { Stack } from 'expo-router';
+import { Link, Stack } from 'expo-router';
+import { Button } from 'react-native-paper';
 
 
 export default function Layout() {
 	return (
-		<Stack screenOptions={{ contentStyle: { backgroundColor: '#283444' } }}>
+		<Stack
+			screenOptions={{
+				contentStyle: { backgroundColor: '#283444' },
+				headerRight: (props) => <Link {...props} href={`/`}><Button icon='home'>Home</Button></Link>
+			}}>
 			<Stack.Screen name='edit' options={{ title: 'Edit Set', headerBackButtonMenuEnabled: true }} />
 			<Stack.Screen name='test' options={{ title: 'Test Set', headerBackButtonMenuEnabled: true }} />
 			<Stack.Screen name='index' options={{ title: 'Set Details', headerBackButtonMenuEnabled: true }} />
